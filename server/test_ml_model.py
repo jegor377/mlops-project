d02fee2cd572e84325b15d7ec976792032a41951
Igor Santarek <igor.santarek@gmail.com>
2025-12-07 13:42:51 +0100

    new model tests and speed tests

---
+++ server/test_ml_model.py
@@ -0,0 +1,20 @@
+import ml_model
+
+def test_correct_prediction():
+    model = ml_model.Model()
+    texts = [
+        "This is the worst experience I've ever had.",
+        "It's a bad product.",
+        "It's neutral.",
+        "The product is good and meets my expectations.",
+        "I love this product! It's amazing and works perfectly.",
+    ]
+    correct_predictions = [
+        "Very Negative",
+        "Negative",
+        "Neutral",
+        "Positive",
+        "Very Positive",
+    ]
+    predictions = model.predict(texts)
+    assert predictions == correct_predictions
