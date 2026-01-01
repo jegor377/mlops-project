5c7d71ea381b82d57d6b2df6c7a1c12cbbdcaee9
Igor Santarek <igor.santarek@gmail.com>
2026-01-01 03:42:47 +0100

    adds comment

--- server/src/server/main.py
+++ server/src/server/main.py
@@ -4,7 +4,7 @@ from .models.data_models import ModelRequest, ModelResponse
 from .services.ml_model import Model
 
 app = FastAPI()
-model = Model()
+model = Model()  # Load the ML model
 
 
 @app.post("/predict", response_model=ModelResponse)
