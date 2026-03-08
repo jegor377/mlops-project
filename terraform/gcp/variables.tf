615d2db06f1a569ed0249a50da3880a2d72f6fb5
Igor Santarek <igor.santarek@gmail.com>
2026-03-08 13:55:50 +0100

    chore: update the region and zone for faster provisioning

--- terraform/gcp/variables.tf
+++ terraform/gcp/variables.tf
@@ -1,11 +1,11 @@
 variable "region" {
   type        = string
-  default     = "europe-central2"
+  default     = "us-central1"
   description = "GCP cluster region"
 }
 
 variable "zone" {
   type        = string
-  default     = "europe-central2-b"
+  default     = "us-central1-a"
   description = "GCP cluster zone"
 }
